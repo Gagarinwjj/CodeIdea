60b6833445c3032588dea5e2a35b13d8fd1f9b52
Gagarinwjj <1045399273@qq.com>
2014-10-10 13:47:50 +0800

    Update AESHelper.java

--- Java/AESHelper.java
+++ Java/AESHelper.java
@@ -1,4 +1,3 @@
-```java
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
@@ -180,4 +179,3 @@ public class AESHelper {
 	}
 
 }
-```
